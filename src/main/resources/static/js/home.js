92fc4de624e22c450a3c56f1818a6daec4876bd3
unknown <sergiohg230796@gmail.com>
2019-10-27 02:09:58 +0100

    last

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -2,10 +2,10 @@ var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center : {
-			lat : -34.397,
-			lng : 150.644
+			lat : 39.9941356,
+			lng : -0.07112801
 		},
-		zoom : 8
+		zoom : 13
 	});
 }
 
@@ -26,4 +26,27 @@ $('#opener').on('click', function() {
 		$('#mapShadow').css('z-index', '0');
 	}	
 	return false;	
+});
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Reciclador', 'No reciclador'],
+        datasets: [{
+            data: [415790, 153785],
+            backgroundColor: [
+                'rgba(116, 6, 180, 1)',
+                'rgba(207, 207, 207, 1)'
+            ],
+            borderColor: [
+                'rgba(116, 6, 180, 0.2)',
+                'rgba(207, 207, 207, 0.2)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+    	responsive : false
+    }
 });
